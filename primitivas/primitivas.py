70b145392a102f4076e02a12151b2f8508f3e966
wera <69810414+natlujan@users.noreply.github.com>
2022-01-27 10:16:40 -0700

    Update primitivas.py

--- primitivas/primitivas.py
+++ primitivas/primitivas.py
@@ -1,41 +1,108 @@
 #Primitivas:
 #Se refiere al esquema interpretativo utilizado por OpenGL
 #para determinar lo que representa un arreglo de vértices 
-#al ser rendereado
+#al ser renderizado
 
 #Importar librerias
-
 from OpenGL.GL import *
 from glew_wish import *
 import glfw
 
-def draw():
+def draw_triangles():
     glBegin(GL_TRIANGLES)
-    #3 vertices que openGL interpreta como triangulo
-    glColor3f(0.7,0.7,0)
+
+    #3 vertices que openGL interpreta como triángulo
+    glColor3f(0.6,0.6,0)
     glVertex3f(-1,0,0)
     glColor3f(0,1,1)
     glVertex3f(0,1,0)
     glColor3f(1,0,1)
     glVertex3f(0.7,0.5,0)
+    glEnd()
 
-    glColor3f(0.7,0.7,0)
-    glVertex3f(-1,0,0)
-    glColor3f(0,1,1)
-    glVertex3f(0,-1,0)
-    glColor3f(1,0,1)
-    glVertex3f(0.7,0.5,0)
+def draw_point():
+    glBegin(GL_POINTS)
+    glColor3f(1.0,1.0,1.0)
+    glVertex(0,0,0)
+    glColor3f(1.0,1.0,1.0)
+    glVertex(-0.5,-0.5,0)
+    glEnd()
 
+def draw_lines():
+    glBegin(GL_LINES)
+    glColor3f(1.0,1.0,1.0)
+    glVertex3f(0.2,-0.8,0.0)
+    glVertex3f(0.8,-0.4,0.0)
+    glEnd()
+
+def draw_line_strip():
+    glBegin(GL_LINE_STRIP)
+    glColor3f(0.5,0.5,0.0)
+    glVertex3f(0.7,-0.5,0.0)
+    glVertex3f(0.9,-0.5,0.0)
+    glVertex3f(0.7,-0.7,0.0)
+    glVertex3f(0.9,-0.7,0.0)
+    glEnd()
+
+def draw_line_loop():
+    glBegin(GL_LINE_LOOP)
+    glColor3f(0.7,0.0,0.7)
+    glVertex3f(-0.6,0.5,0.0)
+    glVertex3f(-0.9,0.3,0.0)
+    glVertex3f(-0.7,0.6,0.0)
+    glEnd()
+
+def draw_triangle_strip():
+    glBegin(GL_TRIANGLE_STRIP)
+    #1
+    glColor3f(0.7,0.0,0.7)
+    glVertex3f(-0.2,-0.8,0.0)
+    #2
+    glColor3f(0.6,0.6,0.0)
+    glVertex3f(-0.6,-0.7,0.0)
+    #3
+    glColor3f(0.5,0.4,0.9)
+    glVertex3f(-0.75,-0.3,0.0)
+    #4
+    glColor3f(0.0,0.7,0.7)
+    glVertex3f(-0.9,-0.9,0.0)
+    glEnd()
+
+def draw_quads():
+    glBegin(GL_QUADS)
+    glColor3f(0.0,0.4,0.7)
+    glVertex3f(-0.2,-0.2,0.0)
+    glColor3f(0.5,0.4,0.9)
+    glVertex3f(0.2,0.2,0.0)
+    glColor3f(1.0,1.0,1.0)
+    glVertex3f(0.2,-0.2,0.0)
+    glColor3f(0.0,0.4,0.7)
+    glVertex3f(-0.2,0.2,0.0)
+    glEnd()
+
+def draw_polygon():
+    glBegin(GL_POLYGON)
+    glColor3f(0.0,0.4,0.7)
+    glVertex3f(0.0,0.0,0.0)
+    glColor3f(0.14,0.4,0.9)
+    glVertex3f(0.0,-0.4,0.0)
+    glColor3f(1.0,1.0,1.0)
+    glVertex3f(0.3,-0.4,0.0)
+    glColor3f(0.0,0.4,0.7)
+    glVertex3f(0.3,0.4,0.0)
+    glColor3f(0.0,-0.4,0.7)
+    glVertex3f(-0.3,0.1,0.0)
     glEnd()
 
 def main():
     width = 700
     height = 700
+    
     #Inicializar GLFW
     if not glfw.init():
         return
 
-    #Declarar ventana
+    #declarar ventana
     window = glfw.create_window(width, height, "Mi ventana", None, None)
 
     #Configuraciones de OpenGL
@@ -45,8 +112,8 @@ def main():
     glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
 
-    #Verificamos la creacion de la ventana
     if not window:
+    #Verificamos la creación de la ventana
         glfw.terminate()
         return
 
@@ -71,21 +138,25 @@ def main():
         #Establecer el viewport
         #glViewport(0,0,800,600)
         #Establecer color de borrado
-
-        glClearColor(0,0.7,0.9,1)
+        glClearColor(0,0.0,0.0,1)
 
         #Borrar el contenido del viewport
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
-
         #Dibujar
-        draw()
+        draw_triangles()
+        draw_point()
+        draw_lines()
+        draw_line_strip()
+        draw_line_loop()
+        draw_triangle_strip()
+        draw_quads()
+        draw_polygon()
 
         #Polling de inputs
         glfw.poll_events()
 
-        #Cambiar los buffers
+        #Cambia los buffers
         glfw.swap_buffers(window)
-    
     glfw.destroy_window(window)
     glfw.terminate()
 
